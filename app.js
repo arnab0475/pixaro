00609036b7220076adca22599a43ec653b52370d
Arnab <arnab2004bera@gmail.com>
2025-08-17 12:08:52 +0530

    fix: remove deprecated MongoDB connection options

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pixaro";
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 var express = require('express');
 var path = require('path');
